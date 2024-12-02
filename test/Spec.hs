3bad5dcd3a634dbfb802f8a79923c2324b85e7b5
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-03 02:31:37 +0300

    add test congruence diff

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import Lib (isCongruent)
+import Test.QuickCheck
+
+-- 1. Если два числа равны по модулю, то их разность делится на модуль
+prop_congruenceDifference :: Int -> Int -> Positive Int -> Bool
+prop_congruenceDifference a b (Positive m) =
+  isCongruent a b m == ((a - b) `mod` m == 0)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "Testing isCongruent"
+  quickCheck prop_congruenceDifference
