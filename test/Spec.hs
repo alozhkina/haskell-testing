b6fb8ad4412f99eed2252a8ceb16d8dc1e196c4f
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-03 03:34:58 +0300

    all tests for isCongruated

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,19 @@ prop_congruenceDifference :: Int -> Int -> Positive Int -> Bool
 prop_congruenceDifference a b (Positive m) =
   isCongruent a b m == ((a - b) `mod` m == 0)
 
+-- 2. Равенство по модулю является симметричным
+prop_congruenceSymmetry :: Int -> Int -> Positive Int -> Bool
+prop_congruenceSymmetry a b (Positive m) =
+  isCongruent a b m == isCongruent b a m
+
+-- 3. Если одно число равно другому, то они равны по любому модулю
+prop_congruenceEquality :: Int -> Positive Int -> Bool
+prop_congruenceEquality a (Positive m) =
+  isCongruent a a m == True
+
 main :: IO ()
 main = do
   putStrLn "Testing isCongruent"
   quickCheck prop_congruenceDifference
+  quickCheck prop_congruenceSymmetry
+  quickCheck prop_congruenceEquality
