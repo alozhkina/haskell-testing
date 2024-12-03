c2650437e2f2d7a6b0b07012ae2ebbc172a2eb55
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-03 03:51:46 +0300

    add isSorted

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,12 @@
-module Lib (isCongruent) where
+module Lib (isCongruent, isSorted) where
 
 -- Проверка равенства по модулю
 isCongruent :: Int -> Int -> Int -> Bool
 isCongruent a b m = (a - b) `mod` m == 0
+
+isSorted :: Ord a => [a] -> Bool -> Bool
+isSorted [] _ = True -- пустой
+isSorted [_] _ = True -- из одного
+isSorted (x:y:xs) arg
+  | arg == True = x <= y && isSorted (y:xs) arg
+  | arg == False = x >= y && isSorted (y:xs) arg
